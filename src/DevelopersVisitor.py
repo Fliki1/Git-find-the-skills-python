345b5e51356df91b62a8d18a92e42b795489b5e9
Flik1 <leo.pelonero@hotmail.it>
2021-08-20 18:09:20 +0200

    :bulb: documenting :zap: class DevelopersVisitor 26%

--- src/DevelopersVisitor.py
+++ src/DevelopersVisitor.py
@@ -1,4 +1,5 @@
 import configparser
+import re
 
 class DevelopersVisitor:
 
@@ -12,6 +13,10 @@ class DevelopersVisitor:
             exstensions = list(CONFIG.get("SkillsSection", type).lower().split(";"))
             self.fileExstensions[type] = exstensions
 
+        self.HTML_PATTERN = "<(\"[^\"]*\"|'[^']*'|[^'\">])*>"
+        # TODO: controllare se [ di apertura sono leciti: leggo che
+        #  In Python you don't need to escape any nested [ but you do need to do that in Java.
+
 
     def getDevelopers(self):
         return self.developers
@@ -21,6 +26,22 @@ class DevelopersVisitor:
         """ return boolean value if str s endwith any of extn entry """
         return any([s.endswith(entry) for entry in extn])
 
+
+    def hasHTMLTags(self, text: str):
+        """ This method matches the regex pattern in the string with the optional flag.
+        It returns true if a match is found in the string otherwise it returns false."""
+        return re.match(self.HTML_PATTERN, text)
+        """ This method returns the match object if there is a match found in the string. """
+        #re.search(regex, text)
+
+
+    def convertToLowerCase(self, strings: []):
+        """ ritorna la lista di stringhe senza spazi agli estremi: strim->strip e in lowercase"""
+        return [string.strip().lower() for string in strings]
+
+
+    def getOnlyAdditions(self):
+        """ lista string di soli ADD dalla mod corrente """
 
     def myfunc(self):
         print(self.CONFIG)
